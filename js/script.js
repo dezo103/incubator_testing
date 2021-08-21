f240436e43177d5305a1e9280de0744c07f79075
Aliaksandr Rudyk <dezo103@gmail.com>
2021-08-21 15:43:40 +0300

    feat: add alert input text

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let inputText = document.querySelector('input');
+let button = document.querySelector('.button');
+button.addEventListener('click', showText);
+
+function showText() {
+    console.log(inputText.value);
+}
+
