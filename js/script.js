621dfa1d0d9033cf480611c201fba02d928f4eb2
Aliaksandr Rudyk <dezo103@gmail.com>
2021-08-21 16:02:29 +0300

    feat: add array of Objects

--- js/script.js
+++ js/script.js
@@ -2,12 +2,28 @@ let inputText = document.querySelector('input');
 let button = document.querySelector('.button');
 button.addEventListener('click', showText);
 
+const arrayOfObject = [
+    {
+        name: 'Alex',
+        age: 45
+    },
+    {
+        name: 'Koss',
+        age: 25},
+    {
+        name: 'Fill',
+        age: 15}
+];
+
 function showText() {
     if (inputText.value === 'google') {
-        alert('Yandex круче. Но это не точно');
+        alert(`Yandex круче. Но это не точно. ${arrayOfObject[0].name}`);
+
     }
     else {
-        alert(inputText.value);
+        alert(inputText.value + '. ' +arrayOfObject[0].name);
     }
 }
+
+
 
